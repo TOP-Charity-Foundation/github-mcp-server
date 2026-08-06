52e08d7c161d0f89e870f9a5849c4347fe65c27a
TOPCharityFoundation <Admin@TOPCharityFoundation.org>
2026-08-06 00:07:13 -0400

    feat(actions): register first governed wrapper tranche

--- pkg/github/inventory.go
+++ pkg/github/inventory.go
@@ -11,8 +11,9 @@ import (
 // Handlers are generated on-demand during registration via RegisterAll(ctx, server, deps).
 // The "default" keyword in WithToolsets will expand to toolsets marked with Default: true.
 func NewInventory(t translations.TranslationHelperFunc) *inventory.Builder {
+	tools := append(AllTools(t), GovernedActionsTools(t)...)
 	return inventory.NewBuilder().
-		SetTools(AllTools(t)).
+		SetTools(tools).
 		SetResources(AllResources(t)).
 		SetPrompts(AllPrompts(t))
 }
